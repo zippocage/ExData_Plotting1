efd8aa78126fb5a2244d71f6f663c0c29e98cd30
Klas Hermodsson <klas@hermodsson.se>
2015-04-11 11:06:52 +0200

    Plot2 part of the project

---
+++ plot2.R
@@ -0,0 +1,27 @@
+library(tidyr)
+library(dplyr)
+
+# load data
+df <- read.table(file = "household_power_consumption.txt", 
+                 header = TRUE, sep = ";", na.strings = "?", 
+                 stringsAsFactors = FALSE)
+
+# convert date and time to full date time column
+df <- mutate(df, timestamp = paste(Date, Time, sep = " "))
+df$timestamp <- strptime(df$timestamp, "%d/%m/%Y %H:%M:%S")
+
+# drop original columns
+df <- select(df, -Date, -Time)
+
+# We will only be using data from the dates 2007-02-01 and 2007-02-02
+df <- df[as.Date(df$timestamp) %in% as.Date(c("2007-02-01", "2007-02-02")),]
+
+
+# time to plot
+png(filename = "plot2.png", width = 480, height = 480)
+
+plot(x = df$timestamp, y = df$Global_active_power, type = "l",
+  main = "", xlab= "", 
+  ylab = "Global Active Power (kilowatts)")
+
+dev.off() ## Don't forget to close the PNG device!
